e9a051db42a878474ba44f224e722f2a5db3cf98
atsushitahira <a.tahira3@icloud.com>
2019-08-07 01:00:35 +0900

    チャンバラゲーム　製作途中
    剣同士の判定と、プレイヤーの体の判定を足したら完成
    あとBGM

---
+++ Assets/PlayerBodySC.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBodySC : MonoBehaviour {
+
+    public static int playerLife = 3;
+    bool isStay = false;
+    [SerializeField] Renderer dameged;
+    float speed = 1;
+    float time;
+    float alfa;
+    float endTime;
+    public GameObject gameoverText;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        playerLife = Mathf.Clamp(playerLife, 0, 3);
+        speed = playerLife == 1 ? 10f : (playerLife == 2 ? 5f : 0f);
+
+        //ゲームオーバー画面にフェードイン
+        if (playerLife == 0)
+        {
+            endTime += Time.deltaTime * 0.4f;
+            dameged.material.color = new Color(0f, 0f, 0f, Mathf.Clamp(endTime,0f,200f));
+            gameoverText.SetActive(true);
+        }
+        else
+        {
+            time += Time.deltaTime * speed;
+            alfa = Mathf.Sin(time) * 0.2f;
+            dameged.material.color = new Color(255f, 0f, 0f, alfa);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<SwordSC>().isRight == false && !isStay)
+        {
+            isStay = true;
+            playerLife--;
+            Debug.Log("PlayerLife is " + playerLife);
+            StartCoroutine("DuplicateProtection");
+            //ジュバっていう音
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    IEnumerator DuplicateProtection()
+    {
+        yield return new WaitForSeconds(1.5f);
+        isStay = false;
+    }
+}
---
+++ Assets/SwordSC.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwordSC : MonoBehaviour {
+
+    public bool isRight;
+    TextMesh debugText;
+	// Use this for initialization
+	void Start () {
+        debugText = GameObject.Find("debugText").GetComponent<TextMesh>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<AudioSource>().Play();
+        if (!this.isRight)
+        {
+            Debug.Log("!isRight in");
+            if (collision.gameObject.tag == "RightSword")
+            {
+                debugText.text = "Collision In";
+                //キィンっていう音
+                GetComponent<AudioSource>().Play();
+                StartCoroutine("Neutralization");
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collision collision)
+    {
+        GetComponent<AudioSource>().Play();
+        if (!this.isRight)
+        {
+            Debug.Log("!isRight in");
+            if (collision.gameObject.tag == "RightSword")
+            {
+                debugText.text = "Collision In";
+                //キィンっていう音
+                GetComponent<AudioSource>().Play();
+                StartCoroutine("Neutralization");
+            }
+        }
+    }
+
+    IEnumerator Neutralization()
+    {
+        isRight = true;
+        Debug.Log("isRight = " + isRight);
+        yield return new WaitForSeconds(1.5f);
+        isRight = false;
+        Debug.Log("isRight = " + isRight);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (!this.isRight)
+        {
+            if (collision.gameObject.GetComponent<SwordSC>().isRight == true)
+            {
+                debugText.text = "Collision Out";
+            }
+        }
+    }
+}
